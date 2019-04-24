bda778400a8397e212cb98b73042f618a99eed9c
thierrylandry <thierrylandryk@gmail.com>
2019-04-24 19:42:23 +0000

    Commit image Eiffage

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid">
             <div class="header-mobile-inner">
                 <a class="logo" href="index.html">
-                    <img src="images/icon/logo.png" alt="CoolAdmin" />
+                    <img src="images/icon/eiffage-black-RGB.png"/>
                 </a>
                 <button class="hamburger hamburger--slider" type="button">
                             <span class="hamburger-box">
@@ -119,7 +119,7 @@
 <aside class="menu-sidebar d-none d-lg-block">
     <div class="logo">
         <a href="#">
-            <img src="images/icon/logo.png" alt="Cool Admin" />
+            <img src="images/icon/eiffage-black-RGB.png" alt="Cool Admin" />
         </a>
     </div>
     <div class="menu-sidebar__content js-scrollbar1">

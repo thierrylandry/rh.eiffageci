1ebedef0cfa09041cbb47fbe1a740447ca6a7a41
ckodia <cyriaquekodia@gmail.com>
2019-08-08 16:36:21 +0000

    afficher le matricule et trier par ordre décroissant

--- resources/views/conges/conges.blade.php
+++ resources/views/conges/conges.blade.php
@@ -42,7 +42,7 @@
                                 <tr>
                                         <td>{{$personne->id}} </td>
                                         <td> <div class="external-event" style="background-color:
-{{$colors[$personne->id]}};color: white">{{$personne->nom.' '.$personne->prenom}}</div></td>
+{{isset($colors[$personne->id])?$colors[$personne->id]:'black'}};color: white">{{$personne->nom.' '.$personne->prenom}}</div></td>
                                         <td>{{isset($personne->contrat_renouvelles()->where('datedebutc','!=',null)->orderBy('datedebutc','ASC')->first()->datedebutc)? date_diff(new DateTime($personne->contrat_renouvelles()->where('datedebutc','!=',null)->orderBy('datedebutc','ASC')->first()->datedebutc),new DateTime('now'))->m:''}}</td>
                                 </tr>
                                     @endforeach

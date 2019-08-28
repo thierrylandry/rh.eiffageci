d59f1aa7305f08e3c313d381f4b8a0925d5d7635
ckodia <cyriaquekodia@gmail.com>
2019-08-28 15:58:59 +0000

    afficher le matricule et trier par ordre décroissant

--- app/Fin_contrat.php
+++ app/Fin_contrat.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Fin_contrat extends Model
 {
     //
+
+
+    
     protected  $table="fin_contrat";
     protected $fillable= ['nom','prenom','libelle','datefinc'];
 }

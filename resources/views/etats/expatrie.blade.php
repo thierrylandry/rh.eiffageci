4208b2f4cd3c1a0c3df9646e45ddb35c8b0a9530
ckodia <cyriaquekodia@gmail.com>
2019-09-24 15:36:58 +0000

    mise a jours des adresses

--- resources/views/etats/expatrie.blade.php
+++ resources/views/etats/expatrie.blade.php
@@ -50,7 +50,18 @@
                             @foreach(json_decode($expatrie->familles) as $familles)
                                 <tr class="tr-shadow">
                                     <td>{{isset($familles->nom_prenom)?explode(' ',$familles->nom_prenom)[0]:''}}</td>
-                                    <td></td>
+                                    <td>
+
+                                        @if(isset($familles->nom_prenom))
+
+                                            @foreach(explode(' ',$familles->nom_prenom) as $tt):
+                                               {{$tt.' '}}
+                                            @endforeach
+
+                                        @endif
+
+
+                                  </td>
                                     <td>{{isset($expatrie->surete) && $expatrie->surete==1?'OUI':'NON'}}</td>
                                 </tr>
                             @endforeach

d859f63ceef3eac1d512046a75417dfd0f4eb1a7
ckodia <cyriaquekodia@gmail.com>
2019-09-25 17:34:34 +0000

    mise a jours des adresses

--- resources/views/etats/expatrie.blade.php
+++ resources/views/etats/expatrie.blade.php
@@ -68,7 +68,7 @@
                                     <td>{{$expatrie->contact}}</td>
                                     <td>{{$expatrie->whatsapp}}</td>
                                     <td>{{$expatrie->sattelitaire}}</td>
-                                    <td>{{$familles->$type_p}} de {{$expatrie->nom}} {{$expatrie->prenom}} </td>
+                                    <td>{{$familles->type_p}} de {{$expatrie->nom}} {{$expatrie->prenom}} </td>
                                 </tr>
                             @endforeach
                             @endif

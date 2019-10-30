d7eec1fd1b15dea2c4f2ecc5288cf6fa233e79f6
ckodia <cyriaquekodia@gmail.com>
2019-10-30 15:59:39 +0000

    total

--- resources/views/etats/expatrie.blade.php
+++ resources/views/etats/expatrie.blade.php
@@ -238,7 +238,7 @@
                     {
                         extend: 'excelHtml5',
                         exportOptions: {
-                            columns:  [ 0,1, 2,3,4,5,6,7,8,,9]
+                            columns:  [ 0,1, 2,3,4,5,6,7,8,9]
                         },
                         text:"Excel",
                         filename: "<?php echo $nom?>",

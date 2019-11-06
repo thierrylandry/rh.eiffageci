c0f4267e6d5c9f19022184fdf6c9b1f7ef7453da
ckodia <cyriaquekodia@gmail.com>
2019-11-06 09:28:28 +0000

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -541,7 +541,7 @@ class HomeController extends Controller
             $vardiagCumul->y=$runningSum;
             $cumule_sortis[] = $vardiagCumul;
         }
-      //  var_dump($cumule_entrees);
+        var_dump($cumule_entrees);
 
         $effectif_par_mois= Array();
 

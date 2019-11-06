5d510097cc702411225226403fb4503232027be9
ckodia <cyriaquekodia@gmail.com>
2019-11-06 09:27:04 +0000

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -553,7 +553,7 @@ class HomeController extends Controller
             $effectif_par_mois[]=$vardiagEffectif;
         }
 
-        //dd($effectif_par_mois);
+        dd($effectif_par_mois);
 
         $entites=Entite::all();
         $lentite=Entite::find($id);

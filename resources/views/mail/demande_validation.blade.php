283244d9387d6c4e263333a8f38920f556e6dcb1
tkoffi <thierrylandryk@gmail.com>
2020-01-28 16:41:20 +0000

    Corps du mail

--- resources/views/mail/demande_validation.blade.php
+++ resources/views/mail/demande_validation.blade.php
@@ -1,19 +1,20 @@
 <p>Bonjour,</p>
 
-<p>Vous avez une nouvelle demande à valider  <br><br>
+<p>Vous avez une nouvelle demande à valider.<br><br>
 
 Cliquez ici pour consulter 👉 : {{$lien}}
 
-<p>Dans l’attente, et en vous remerciant par avance,<br>
-    <br>
+<p>Dans l’attente, et en vous remerciant par avance,<br><br>
+
 <p>
     Cordialement,
     <br>
-    PRO-RH,
 </p>
+
 </p>
 
 <p>
+    <strong>PRO-RH,</strong></br>
     <strong>Eiffage Génie Civil Côte d’Ivoire</strong></br>
     <img src="http://172.20.73.3/achat.eiffageci/images/logomail.png"/>
 </p>

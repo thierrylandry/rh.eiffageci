3d34dc40b5dfd0a8c6f66f8aca59aa7a2fc9a10c
iceberglca <cyriaquekodia@gmail.com>
2020-02-25 16:47:38 +0000

    correction bug

--- app/Http/Controllers/CongerController.php
+++ app/Http/Controllers/CongerController.php
@@ -606,7 +606,7 @@ class CongerController extends Controller
         return redirect()->back()->with('success',"La demande d'Absconge a été  supprimée avec succès");
     }
     public function validation_conges(){
-        $conges = DB::table('absconges')->Join('personne','personne.id','=','absconges.id_personne')->where('service','=',Auth::user()->id_service)->where('etat','=',1)->get();
+        $conges = DB::table('absconges')->Join('personne','personne.id','=','absconges.id_personne')->Join('Type_conge','Type_conge.id','=','absconges.id_motif_demande')->where('service','=',Auth::user()->id_service)->where('etat','=',1)->get();
       //  dd($conges);
         $mode="validation";
         $entites=Entite::all();

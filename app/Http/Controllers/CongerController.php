ad0a67f03a29d9b11ceefcbd6b6a9eef24df6343
iceberglca <cyriaquekodia@gmail.com>
2020-02-26 10:20:18 +0000

    correction bug

--- app/Http/Controllers/CongerController.php
+++ app/Http/Controllers/CongerController.php
@@ -606,14 +606,16 @@ class CongerController extends Controller
         return redirect()->back()->with('success',"La demande d'Absconge a été  supprimée avec succès");
     }
     public function validation_conges(){
+
         $conges = DB::table('absconges')
             ->leftJoin('type_conges','type_conges.id','=','absconges.id_personne')
             ->leftJoin('personne','personne.id','=','absconges.id_motif_demande')
             ->leftJoin('users','users.id','=','absconges.id_users')->where('etat','=',1)
             ->where('personne.service','=',Auth::user()->id_service)
             ->where('personne.id','!=',Auth::user()->id_personne)
-            ->select('absconges.id','jour','solde','debut','fins','reprise','adresse_pd_conges','contact_telephonique','etat','libelle as libelle_type_conges','users.nom as nom_users','users.prenoms as prenoms_users','personne.slug')->get();
-      //  dd($conges);
+            ->select('absconges.id','jour','solde','debut','fins','reprise','adresse_pd_conges','contact_telephonique','etat','libelle as libelle_type_conges','users.nom as nom_users','users.prenoms as prenoms_users','personne.slug','personne.service')->get();
+        dd($conges);
+        //  dd($conges);
         $mode="validation";
         $entites=Entite::all();
 

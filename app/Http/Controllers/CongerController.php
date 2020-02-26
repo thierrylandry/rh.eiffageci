e2c274f77e2e298f10c26805113d25ce274fee28
iceberglca <cyriaquekodia@gmail.com>
2020-02-26 10:21:29 +0000

    correction bug

--- app/Http/Controllers/CongerController.php
+++ app/Http/Controllers/CongerController.php
@@ -614,7 +614,7 @@ class CongerController extends Controller
             ->where('personne.service','=',Auth::user()->id_service)
             ->where('personne.id','!=',Auth::user()->id_personne)
             ->select('absconges.id','jour','solde','debut','fins','reprise','adresse_pd_conges','contact_telephonique','etat','libelle as libelle_type_conges','users.nom as nom_users','users.prenoms as prenoms_users','personne.slug','personne.service')->get();
-        dd($conges);
+       // dd($conges);
         //  dd($conges);
         $mode="validation";
         $entites=Entite::all();

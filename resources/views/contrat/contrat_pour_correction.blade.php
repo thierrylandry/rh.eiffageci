87c5c43a5e6ba84d57e0a8eb63672cf06c4686b2
iceberglca <cyriaquekodia@gmail.com>
2020-02-28 16:53:27 +0000

    correction bug sur le lien

--- resources/views/contrat/contrat_pour_correction.blade.php
+++ resources/views/contrat/contrat_pour_correction.blade.php
@@ -82,7 +82,7 @@
         <a  href="{{route('fiche_personnel',$personne->slug)}}" class="btn btn-outline-primary">Consulter la fiche</a>
         <a  href="{{route('detail_personne',$personne->slug)}}" class="btn btn-outline-secondary">Modifier les informations</a>
         <a href="{{route('document_administratif',$personne->slug)}}" class="btn btn-outline-success"> gérer les dossiers</a>
-        <a href="{{route('lister_contrat',$personne->slug)}}" class="btn btn-outline-danger">Gérer les contrats</a>
+        <a href="{{route('lister_contrat',$personne->id)}}" class="btn btn-outline-danger">Gérer les contrats</a>
     </div>
     <form action="{{route('save_correction_contrat')}}" method="post" enctype="multipart/form-data" class="form-horizontal">
         @csrf

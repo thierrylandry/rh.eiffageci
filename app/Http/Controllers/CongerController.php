e8fe8ddc11a8233af2043269151d42bc8ca1cc60
iceberglca <cyriaquekodia@gmail.com>
2020-03-04 10:22:24 +0000

    correction bug sur le lien

--- app/Http/Controllers/CongerController.php
+++ app/Http/Controllers/CongerController.php
@@ -630,8 +630,9 @@ class CongerController extends Controller
             $conges = DB::table('absconges')
                 ->leftJoin('type_conges','type_conges.id','=','absconges.id_motif_demande')
                 ->leftJoin('personne','personne.id','=','absconges.id_personne')
-                ->leftJoin('users','users.id','=','absconges.id_users')->where('etat','=',1)
-              
+                ->leftJoin('contrat','personne.id','=','contrat.id_personne')->where('contrat.etat','=',1)
+                ->leftJoin('users','users.id','=','absconges.id_users')->where('absconges.etat','=',1)
+                ->where('contrat.service','=',Auth::user()->id_service)
                 ->where('personne.id','!=',Auth::user()->id_personne)
                 ->select('absconges.id','jour','solde','debut','fins','reprise','adresse_pd_conges','contact_telephonique','etat','libelle as libelle_type_conges','users.nom as nom_users','users.prenoms as prenoms_users','personne.slug','personne.service','personne.nom','personne.prenom')->get();
 

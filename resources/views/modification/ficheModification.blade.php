78db963a3a5cb3d1836cc5b81c0e687b014baf39
iceberglca <cyriaquekodia@gmail.com>
2020-03-06 10:43:49 +0000

    correction bug sur le lien

--- resources/views/modification/ficheModification.blade.php
+++ resources/views/modification/ficheModification.blade.php
@@ -401,8 +401,8 @@ function auchangement(){
             $("#dm_id_categorie").append(lesOptions);
         });
 
-        $("#dm_id_categorie").val(data[0].id_categorie);
         $("#dm_id_categorie_initial").val(data[0].id_categorie);
+        $("#dm_id_categorie").val(data[0].id_categorie);
 
     });
 }

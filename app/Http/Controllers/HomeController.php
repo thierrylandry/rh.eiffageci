8925071e4bec6a7c7696662fbc2c4cb9a0a9f23e
iceberglca <cyriaquekodia@hotmail.fr>
2020-06-03 16:08:09 +0000

    correction bug sur le lien

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,7 +46,6 @@ class HomeController extends Controller
         $effectifglobaux_tab=DB::table('personne_presente')
             ->leftJoin('unite','unite.id_unite','=','personne_presente.id_unite')
             ->leftJoin('contrat','contrat.id_personne','=','personne_presente.id')
-            ->leftJoin('entite','personne_presente.id_entite','=','entite.id')
         //       ->whereBetween(DB::raw('CAST(NOW() AS DATE)'), array(DB::raw('contrat.datedebutc'), DB::raw('contrat.datefinc')))
             ->where('id_entite','=',$id_entite_connecter)
             ->groupBy('unite.id_unite','id_entite')

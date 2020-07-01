5de7a5a7482fa9ae0819407059d218cfcbabe0ea
iceberglca <cyriaquekodia@hotmail.fr>
2020-07-01 20:45:26 +0000

    envoie de mail fin contrat par projet.

--- app/Http/Controllers/AbsenceController.php
+++ app/Http/Controllers/AbsenceController.php
@@ -281,8 +281,6 @@ class AbsenceController extends Controller
                 ->leftJoin('roles','user_role.role_id','=','roles.id')
                 ->orWhere('roles.name','=','Chef_de_service')->where('absence.etat','=',1) ->where('personne.id_entite','=',Auth::user()->id_chantier_connecte)
                 ->select('absence.id','jour','debut','fin','reprise','etat','users.nom as nom_users','users.prenoms as prenoms_users','personne.slug','personne.nom','personne.prenom')->get();
-
-       dd($absences);
         }else{
             $absences = DB::table('absence')
                 ->leftJoin('type_permission','type_permission.id','=','absence.id_personne')

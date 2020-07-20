d268dfd92167df881020954bf3a441b1d61de96a
iceberglca <cyriaquekodia@hotmail.fr>
2020-07-20 11:14:29 +0000

    avenant dotation en nature

--- resources/views/contrat/avenant.blade.php
+++ resources/views/contrat/avenant.blade.php
@@ -212,7 +212,7 @@
             @endswitch
 
 
-            @endforeach à compter du <?php if(isset($contrat->date_debutc_eff)){$date = new DateTime($contrat->date_debutc_eff);
+            @endforeach </br>A compter du <?php if(isset($contrat->date_debutc_eff)){$date = new DateTime($contrat->date_debutc_eff);
                 echo $date->format('d-m-Y');}?>.
     @endif
     <br>

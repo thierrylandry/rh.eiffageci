276469f5136fe776fe48111218d1204b63641799
iceberglca <cyriaquekodia@hotmail.fr>
2020-09-03 17:22:32 +0000

    collaborateur

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -493,10 +493,10 @@ $tab = Array();
             $vardiagEffectif1->y=$valeur;
             $effectif_par_mois[]=$vardiagEffectif1;
             $tab[][$lelibelle.' venu']="arrivé ".$this->donne_moi_une_date_je_te_dis_qui_est_venu($cumule_entrees,$lelibelle)."; sorti=".$this->compte_sortie($personne_sortie_unique,$lelibelle)." cumule :".$valeur;
-            $tab[][$lelibelle.' sortie']=$this->compte_sortie($personne_sortie_unique,$lelibelle);
+           // $tab[][$lelibelle.' sortie']=$this->compte_sortie($personne_sortie_unique,$lelibelle);
           //  $tab[][$lelibelle.' valeur']=$valeur;
         }
-      //  dd($tab);
+        dd($tab);
         $effectif_par_mois_le_plus_ressent =Array();
         $i=11;
         while($i>=0){

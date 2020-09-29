0c97b1015b1c1f23557e9796ccd4320a8fd9439d
iceberglca <cyriaquekodia@hotmail.fr>
2020-09-29 12:58:35 +0000

    probleme

--- resources/views/contrat/contratcddpdf.blade.php
+++ resources/views/contrat/contratcddpdf.blade.php
@@ -243,7 +243,10 @@
     <p>L’Employé percevra conformément à sa catégorie professionnelle une rémunération mensuelle brute de <b class="classtext"><?php $affiche=0;
             if(isset($contrat->valeurSalaire)){
                 foreach(json_decode($contrat->valeurSalaire) as $valeurSalaire):
-                    $affiche+=$valeurSalaire->valeur;
+                    if($valeurSalaire!=""){
+                        $affiche+=$valeurSalaire->valeur;
+                    }
+
                 endforeach;
                 echo $affiche;
             }

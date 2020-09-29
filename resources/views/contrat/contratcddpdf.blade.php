587a3214b387aa58f00baca02da226b6d3c728e2
iceberglca <cyriaquekodia@hotmail.fr>
2020-09-29 13:00:06 +0000

    collaborateur

--- resources/views/contrat/contratcddpdf.blade.php
+++ resources/views/contrat/contratcddpdf.blade.php
@@ -243,7 +243,7 @@
     <p>L’Employé percevra conformément à sa catégorie professionnelle une rémunération mensuelle brute de <b class="classtext"><?php $affiche=0;
             if(isset($contrat->valeurSalaire)){
                 foreach(json_decode($contrat->valeurSalaire) as $valeurSalaire):
-                    if($valeurSalaire!=""){
+                    if($valeurSalaire->valeur!=""){
                         $affiche+=$valeurSalaire->valeur;
                     }
 

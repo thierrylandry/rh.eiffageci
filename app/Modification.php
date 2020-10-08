9c532fb8e952bbbc6ff1c0af477058ccee2a1f28
iceberglca <cyriaquekodia@hotmail.fr>
2020-10-08 16:47:04 +0000

    collaborateur

--- app/Modification.php
+++ app/Modification.php
@@ -32,7 +32,7 @@ class Modification extends Model
         return $this->belongsTo(Categorie::class, "id","id_categorie");
     }
     public function fonction(){
-        return $this->belongsTo(Fonction::class,"id_fonction", "id");
+        return $this->belongsTo(Fonction::class, "id","id_fonction",);
     }
     public function fonction_initial(){
         return $this->belongsTo(Fonction::class,"id_fonction_initial", "id");

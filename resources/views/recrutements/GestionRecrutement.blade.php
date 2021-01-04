61a981942fa81f85effb803be9766ea852a1b00f
iceberglca <cyriaquekodia@hotmail.fr>
2021-01-04 11:47:06 +0000

    collaborateur

--- resources/views/recrutements/GestionRecrutement.blade.php
+++ resources/views/recrutements/GestionRecrutement.blade.php
@@ -179,7 +179,7 @@
 
         $(document).ready(function() {
             var table= $('#table_recrutement').DataTable({
-                "order": [[ 0, "desc" ]],
+                "order": [[ 2, "desc" ]],
                 dom: 'Bfrtip',
                 buttons: [
                     'copy', 'csv', 'excel', 'pdf', 'print'

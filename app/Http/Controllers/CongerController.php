080313760fdc2eaae3fd91629e8a4b9b72153211
iceberglca <cyriaquekodia@hotmail.fr>
2021-01-27 13:49:09 +0000

    collaborateur

--- app/Http/Controllers/CongerController.php
+++ app/Http/Controllers/CongerController.php
@@ -645,7 +645,7 @@ class CongerController extends Controller
                 ->Join('roles','user_role.role_id','=','roles.id')
                 ->where('absconges.etat','=',1)
                 ->where('personne.id_entite','=',Auth::user()->id_chantier_connecte)
-                ->orwhere(['roles.name','=','Chef_de_service'])
+                ->orWhere([['roles.name','=','Chef_de_service']])
                 ->where([['contrat.id_service','=',Auth::user()->id_service],['absconges.etat','=',1]])
             //    ->where('personne.id','!=',Auth::user()->id_personne)
                 ->select('absconges.id','jour','solde','debut','fins','reprise','adresse_pd_conges','contact_telephonique','absconges.etat','libelle as libelle_type_conges','users.nom as nom_users','users.prenoms as prenoms_users','personne.slug','personne.service','personne.nom','personne.prenom')->distinct()->get();

e0d87f5266a624747e0a3ea4039f1aed7e26e69d
iceberglca <cyriaquekodia@hotmail.fr>
2021-01-27 14:13:54 +0000

    collaborateur

--- app/Http/Controllers/CongerController.php
+++ app/Http/Controllers/CongerController.php
@@ -637,18 +637,19 @@ class CongerController extends Controller
 
         if(Auth::user()->hasRole('Chef_de_projet')){
             $conges = DB::table('absconges')
-                ->leftJoin('type_conges','type_conges.id','=','absconges.id_motif_demande')
-                ->leftJoin('personne','personne.id','=','absconges.id_personne')
-                ->leftJoin('contrat','personne.id','=','contrat.id_personne')
-                ->leftJoin('users','users.id','=','absconges.id_users')
+                ->Join('type_conges','type_conges.id','=','absconges.id_motif_demande')
+                ->Join('personne','personne.id','=','absconges.id_personne')
+                ->Join('contrat','personne.id','=','contrat.id_personne')
+                ->Join('users','users.id','=','absconges.id_users')
                 ->leftJoin('user_role','user_role.user_id','=','users.id')
                 ->Join('roles','user_role.role_id','=','roles.id')
                 ->where('absconges.etat','=',1)
                 ->where('personne.id_entite','=',Auth::user()->id_chantier_connecte)
-
-
+                ->where('roles.name','=','Chef_de_service')
+                ->orwhere([['contrat.id_service','=',Auth::user()->id_service],['absconges.etat','=',1]])
             //    ->where('personne.id','!=',Auth::user()->id_personne)
                 ->select('absconges.id','jour','solde','debut','fins','reprise','adresse_pd_conges','contact_telephonique','absconges.etat','libelle as libelle_type_conges','users.nom as nom_users','users.prenoms as prenoms_users','personne.slug','personne.service','personne.nom','personne.prenom')->distinct()->get();
+
             $conges_valides_par_mois = DB::table('absconges')
                 ->Join('type_conges','type_conges.id','=','absconges.id_motif_demande')
                 ->Join('personne','personne.id','=','absconges.id_personne')

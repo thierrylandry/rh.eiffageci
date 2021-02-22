03b9dade9155be18982c04fefa9d68a563944ac7
iceberglca <cyriaquekodia@hotmail.fr>
2021-02-22 10:25:17 +0000

    collaborateur

--- resources/views/etats/fin_contrat_service.blade.php
+++ resources/views/etats/fin_contrat_service.blade.php
@@ -107,8 +107,8 @@
                                 <td>{{$fincontrat_traite->nom}}</td>
                                 <td>{{$fincontrat_traite->prenom}}</td>
                                 <td>{{$fincontrat_traite->libelle}}</td>
-                                <td><i class="fa fa-calendar-times-o" aria-hidden="true"></i>{{\Carbon\Carbon::parse($contrat->datedebutc)->format('d-m-Y')}}</td>
-                                <td><i class="fa fa-calendar-times-o" aria-hidden="true"></i>{{\Carbon\Carbon::parse($contrat->datefinc)->format('d-m-Y')}}</td>
+                                <td><i class="fa fa-calendar-times-o" aria-hidden="true"></i>{{\Carbon\Carbon::parse($fincontrat_traite->datedebutc)->format('d-m-Y')}}</td>
+                                <td><i class="fa fa-calendar-times-o" aria-hidden="true"></i>{{\Carbon\Carbon::parse($fincontrat_traite->datefinc)->format('d-m-Y')}}</td>
                                 <td>@if($fincontrat_traite->etat==0)&#128553; Non renouvellé @else &#128525; Renouvellé@endif </td>
                             </tr>
                     @endforeach
